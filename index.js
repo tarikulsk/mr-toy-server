5b138fee3d31506f4c6124a061d2cc134b144cd8
tarikulsk <tarikulsk4@gmai.com>
2023-06-15 23:24:15 +0530

    final

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ const port = process.env.port || 5000;
 console.log(process.env.DB_PASS);
 
 const alltoys = require('./data/toys.json')
-const ironman = require('./data/category/ironman.json')
-const spiderman = require('./data/category/spiderman.json')
-const galaxy = require('./data/category/galaxy.json')
+
 
 // const toys
 app.get('/', (req, res) => {
@@ -110,25 +108,11 @@ run().catch(console.dir);
 
 
 
-
-
-
-
-
 app.get('/alltoys', (req, res) => {
     res.send(alltoys)
 })
 
 
-app.get('/ironman', (req, res) => {
-    res.send(ironman)
-})
-app.get('/spiderman', (req, res) => {
-    res.send(spiderman)
-})
-app.get('/galaxy', (req, res) => {
-    res.send(galaxy)
-})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
